05cd6eb954cec3963148115cd053b41b4f77839b
codeTIT4N <lokeshkumar636@yahoo.com>
2023-04-11 02:58:36 +0530

    updates

--- src/cli.ts
+++ src/cli.ts
@@ -1,23 +1,41 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
-// import * as path from "path";
+import generateProject from "./generateProject.ts";
 import fs from "fs";
 import * as url from "url";
-const __filename = url.fileURLToPath(import.meta.url);
+const CURR_DIR = process.cwd();
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
 export async function main() {
   const CHOICES = fs.readdirSync(__dirname + "../templates");
 
-  console.log(CHOICES[0]);
-  /*const answers = await inquirer.prompt([
+  const QUESTIONS = [
     {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
+      name: "template",
+      type: "list",
+      message: "Choose a template",
+      choices: CHOICES,
     },
-  ]);
-  console.log(answers);*/
+    {
+      name: "projectName",
+      type: "input",
+      message: "Project name:",
+      validate: function (input: string) {
+        if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
+        else
+          return "Project name may only include letters, numbers, underscores and hashes.";
+      },
+    },
+  ];
+  inquirer.prompt(QUESTIONS).then((answers) => {
+    const projectChoice: string = answers["template"];
+    const projectName: string = answers["projectName"];
+    const templatePath = `${__dirname}/../templates/${projectChoice}`;
+
+    fs.mkdirSync(`${CURR_DIR}/${projectName}`);
+
+    generateProject(templatePath, projectName);
+  });
 }
 
 main();
