faa21dadbe8a23b176d5b1a26138881196f3f92c
codeTIT4N <lokeshkumar636@yahoo.com>
2023-04-12 22:38:44 +0530

    small change

--- src/cli.ts
+++ src/cli.ts
@@ -5,7 +5,33 @@ import * as url from "url";
 const CURR_DIR = process.cwd();
 const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-export async function main() {
+function generateProject(templatePath: string, newProjectPath: string) {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach((file) => {
+    const origFilePath = `${templatePath}/${file}`;
+
+    // get stats about the current file
+    const stats = fs.statSync(origFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(origFilePath, "utf8");
+
+      // Rename
+      if (file === ".npmignore") file = ".gitignore";
+
+      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
+      fs.writeFileSync(writePath, contents, "utf8");
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
+
+      // recursive call
+      generateProject(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+    }
+  });
+}
+
+(() => {
   const CHOICES = fs.readdirSync(__dirname + "../templates");
 
   const QUESTIONS = [
@@ -35,32 +61,4 @@ export async function main() {
 
     generateProject(templatePath, projectName);
   });
-}
-
-function generateProject(templatePath: string, newProjectPath: string) {
-  const filesToCreate = fs.readdirSync(templatePath);
-
-  filesToCreate.forEach((file) => {
-    const origFilePath = `${templatePath}/${file}`;
-
-    // get stats about the current file
-    const stats = fs.statSync(origFilePath);
-
-    if (stats.isFile()) {
-      const contents = fs.readFileSync(origFilePath, "utf8");
-
-      // Rename
-      if (file === ".npmignore") file = ".gitignore";
-
-      const writePath = `${CURR_DIR}/${newProjectPath}/${file}`;
-      fs.writeFileSync(writePath, contents, "utf8");
-    } else if (stats.isDirectory()) {
-      fs.mkdirSync(`${CURR_DIR}/${newProjectPath}/${file}`);
-
-      // recursive call
-      generateProject(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
-    }
-  });
-}
-
-main();
+})();
